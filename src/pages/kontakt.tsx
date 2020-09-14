a16df1600fd20f874a5d383704f27ea132d63edf
Masiu <ciszekmarcell@gmail.com>
2020-09-14 19:07:02 +0200

    route path

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -25,7 +25,7 @@ const WorkWithUsPage: React.FC<PageProps<WorkWithUsPageProps>> = ({ data }) => {
           className="kontakta-oss-title"
           title="Kontakta oss"
           isCta
-          ctaPath="arbeta-med-oss"
+          ctaPath="arbeta-for-oss"
         />
       </Hero>
       <Page>
